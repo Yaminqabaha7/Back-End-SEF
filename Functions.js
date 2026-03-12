70d5791119a191955c326ba2872c76769f92545b
yamin <yaminrehan15@gmail.com>
2026-03-12 17:06:02 +0200

    Lec4

--- Functions.js
+++ Functions.js
@@ -1,16 +1,63 @@
 const fs = require("fs");
 
-const add = (fname, lname, age, city) => {
+const add = (fname, lname, age, city, id) => {
   const data = loadData();
-  data.push({
-    fname,
-    lname,
-    age,
-    city,
-  });
-  saveData(data);
+  const duplicate = data.find((item) => item.id === id); // to check if the id already exists in the data array, it returns the first item that matches the condition or undefined if no item matches
+  if (!duplicate) {
+    // if the id does not exist, it adds the new item to the data array and saves it to the file
+    data.push({
+      fname,
+      lname,
+      age,
+      city,
+      id,
+    });
+    saveData(data);
+  } else {
+    console.log("ID already exists");
+  }
+};
+///////////////////////////////////////////////
+
+const deleteItem = (id) => {
+  const data = loadData();
+  const newData = data.filter((item) => item.id !== id); // to create a new array that contains all the items except the one with the specified id
+
+  saveData(newData);
+
+  console.log("Item deleted"); // to save the new array to the file and print a message to the console
 };
 
+// const deleteItem = (fname) => {
+//   const data = loadData();
+//   const newData = data.filter((item) => item.fname !== fname); // to create a new array that contains all the items except the one with the specified id
+
+//   saveData(newData); // to save the new array to the file, it will overwrite the existing data in the file
+
+//   console.log("Item deleted"); // to log a message to the console to indicate that the item has been deleted
+// };
+
+///////////////////////////////////////////////
+
+const readedate = (id) => {
+  const data = loadData();
+  const item = data.find((item) => item.id === id); // to find the item with the specified id in the data array, it returns the first item that matches the condition or undefined if no item matches
+  if (item) {
+    console.log(item); // to log the item to the console if it exists or log a message if it does not exist
+  } else {
+    console.log("Item not found");
+  }
+};
+///////////////////////////////////////////////
+
+const labled = () => {
+  const data = loadData();
+  data.forEach((item) => {
+    console.log(
+      `First name: ${item.fname}, Last name: ${item.lname}, Age: ${item.age}, City: ${item.city}, ID: ${item.id}`
+    ); // to log each item in the data array to the console in a formatted way
+  });
+};
 
 ///////////////////////////////////////////////
 const loadData = () => {
@@ -20,16 +67,16 @@ const loadData = () => {
   } catch {
     return [];
   }
-};// to load the data from the file and parse it to an array of objects, if the file does not exist or is empty, it returns an empty array
-
-
+}; // to load the data from the file and parse it to an array of objects, if the file does not exist or is empty, it returns an empty array
 
 const saveData = (data) => {
   const dataString = JSON.stringify(data);
   fs.writeFileSync("data.json", dataString);
-};// to convert the array of objects to a string and write it to the file
-
+}; // to convert the array of objects to a string and write it to the file
 
 module.exports = {
   add,
+  delete: deleteItem,
+  readedate,
+  labled,
 };
--- file1.js
+++ file1.js
@@ -1,32 +1,14 @@
-//////////////////// Lec 3 ////////////////////
+//////////////////// Lec 4 ////////////////////
 
 ///////////////////////////////////////
 const fs = require("fs");
 
-const yamin = {
-  name: "Yamin",
-  age: 25,
-};
-
-console.log(yamin);
-
-const yaminString = JSON.stringify(yamin); /// converts object to string(json format)
-
-console.log(yaminString);
-
-const yaminParsed = JSON.parse(yaminString); /// converts string(Json) back to object
-console.log(yaminParsed);
-
-fs.writeFileSync("yamin.json", yaminString); /// writes the string(json) to a file
-
-//////////////////////////////////////////////////////////////
-
 const yargs = require("yargs");
-const { hideBin } = require("yargs/helpers");// to hide the first two arguments (node and file name) from the command line arguments
+const { hideBin } = require("yargs/helpers"); // to hide the first two arguments (node and file name) from the command line arguments
 
 const argv = yargs(hideBin(process.argv));
 
-const Functions = require("./Functions");// to import the functions from the Functions.js file
+const Functions = require("./Functions"); // to import the functions from the Functions.js file
 
 argv.command({
   command: "add",
@@ -41,10 +23,58 @@ argv.command({
       demandOption: true,
     },
   },
-  handler: (x) =>// the handler function that will be executed when the command is called
-   {
-    Functions.add(x.fname, x.lname, x.age, x.city);
+  handler: (
+    x // the handler function that will be executed when the command is called
+  ) => {
+    Functions.add(x.fname, x.lname, x.age, x.city, x.id);
+  },
+});
+///////////////////////////////////////////////////// delete command //////////////////////////////////////////////////
+argv.command({
+  command: "delete",
+  describe: "delete a new note",
+  builder: {
+    id: {
+      describe: "ID",
+      demandOption: true,
+    },
+    // fname: {
+    //   describe: "First name",
+    //   demandOption: true,
+    // }, // to specify the options that the command accepts, it takes an object where the keys are the option names and the values are objects that describe the options, in this case, it has two options: id and fname, where id is not required and fname is required
+  },
+  handler: (
+    x // the handler function that will be executed when the command is called
+  ) => {
+    Functions.delete(x.id);
   },
 });
 
-console.log(argv.argv); // to parse the command line arguments and execute the corresponding command
+///////////////////////////////////////////////////// reade command //////////////////////////////////////////////////
+argv.command({
+  command: "reade",
+  describe: "reade a new note",
+  builder: {
+    id: {
+      describe: "ID",
+      demandOption: true,
+    },
+  },
+  handler: (
+    x // the handler function that will be executed when the command is called
+  ) => {
+    Functions.readedate(x.id);
+  },
+});
+
+////////////////////////////////////////////////////// labled command //////////////////////////////////////////////////
+argv.command({
+  command: "labled",
+  describe: "labled a new note",
+  handler: (
+    x // the handler function that will be executed when the command is called
+  ) => {
+    Functions.labled();
+  },
+});
+argv.argv; // to parse the command line arguments and execute the corresponding command
