8065a58369c66fbc2b75412db425520741d1ca7b
yamin <yaminrehan15@gmail.com>
2026-03-18 23:58:25 +0200

    Lec-6

--- file1.js
+++ file1.js
@@ -1,17 +1,22 @@
-//////////////////// Lec 5 ////////////////////
+//////////////////// Lec 6 ////////////////////
 const request = require("request");
-const url =
-  "http://api.weatherapi.com/v1/current.json?key=65f2598868c24b6d8cf153726261203&q=Palestine&aqi=no";
+const geocode = require("./file1/geocode");
 
-request({ url, json: true }, (err, response) => {
+const forcast = require("./file1/forcast");
+
+ const address = process.argv[2];
+geocode(address, (err, data) => {
   if (err) {
-    console.log("Error Page");
-  } else if (response.body.error) {
-    //check if the API response contains an error
-    console.log(response.body.error.message); //display the error message from the API response
-  } else {
-    console.log(response.body.location.name); //display the location name
-    console.log(response.body.current.temp_c); //display the current temperature in Celsius
-    console.log(response.body.current.condition.text); //display the weather condition
+    return console.log("Error:", err);
   }
+
+  console.log("Geocode Data:", data);
+
+  forcast(data.longitude, data.latitude, (err, forecastData) => {
+    if (err) {
+      return console.log("Error:", err);
+    }
+
+    console.log("Forecast Data:", forecastData);
+  });
 });
