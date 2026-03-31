a13ecfe99fb1ba82b7d508df7d2a7ba1f0ea2467
yamin <yaminrehan15@gmail.com>
2026-03-31 10:45:40 +0200

    Lec-7

--- src/file1.js
+++ src/file1.js
@@ -8,6 +8,24 @@ const port = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.send("Hello World!");
 }); 
+
+app.get("/profile", (req, res) => {
+  res.send("This is the profile page");
+}); 
+
+app.get("/page", (req, res) => {
+  res.send({
+    name: "John Doe",
+    age: 30,
+    email: " yamin  @example.com"
+  });
+}); 
+//////////////////////////////////
+const path = require("path");
+const publicPath = path.join(__dirname, "../public");
+app.use(express.static(publicPath));
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
